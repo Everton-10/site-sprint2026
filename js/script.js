6f533bdd36250e573f6fa921352011543f5be275
Everton-10 <everton.ocasagrande@gmail.com>
2026-01-31 09:42:17 -0300

    Primeiro commit do site

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const track = document.getElementById("carouselTrack");
+let index = 0;
+
+function moveCarousel() {
+  const sponsorWidth = document.querySelector(".sponsor").offsetWidth + 60;
+  index++;
+
+  if (index >= track.children.length) {
+    index = 0;
+  }
+
+  track.style.transform = `translateX(-${index * sponsorWidth}px)`;
+}
+
+setInterval(moveCarousel, 3000);
+
+// filtro pagina categorias//
+
+const input = document.getElementById("searchInput");
+
+input.addEventListener("keyup", function () {
+  const filtro = input.value.toLowerCase();
+  const linhas = document.querySelectorAll(".linha-cat");
+
+  linhas.forEach(linha => {
+    const texto = linha.innerText.toLowerCase();
+    linha.style.display = texto.includes(filtro) ? "" : "none";
+  });
+});
